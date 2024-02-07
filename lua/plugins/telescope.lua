5d3f51b9b7f7a148e209f89c207b5d3c76d0a68b
Ben Tang <sjps30503@gmail.com>
2024-02-07 20:28:36 +0800

    Updated telescope.lu (added a new icon)

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,6 +3,13 @@ return {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.5',
         dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            require("telescope").setup({
+                defaults = {
+                    prompt_prefix = " 󰍉 ",
+                },
+            })
+        end
     },
     {
         "nvim-telescope/telescope-ui-select.nvim",
