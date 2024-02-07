fd346470da60b4c6f0f7fc2dc526ce9fc1d4f8e8
Ben Tang <sjps30503@gmail.com>
2024-02-07 18:57:18 +0800

    Added show all references to keybind.lua

--- lua/keybind.lua
+++ lua/keybind.lua
@@ -67,6 +67,7 @@ vim.keymap.set('n', '<leader>gs', '<Plug>(leap-from-window)', {})
 -- Nvim-lspconfig
 vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
 vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
 vim.keymap.set('n', '<leader>rr', function()
     local position_params = vim.lsp.util.make_position_params()
