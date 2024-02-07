784bbd3f8aa21872f3ce302d0e4438da40684500
Ben Tang <sjps30503@gmail.com>
2024-02-07 21:53:05 +0800

    Updated bqf appearances

--- lua/plugins/better-quickfix.lua
+++ lua/plugins/better-quickfix.lua
@@ -1,4 +1,13 @@
 return {
     'kevinhwang91/nvim-bqf',
     ft = 'qf',
+    config = function()
+        require("bqf").setup({
+            auto_enable = true,
+            auto_resize_height = true,
+            preview = {
+                show_title = false,
+            }
+        })
+    end
 }
