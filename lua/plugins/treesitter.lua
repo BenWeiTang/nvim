4df82917ab7cbe8463ed41481fe9ed628c160877
Ben Tang <sjps30503@gmail.com>
2024-02-07 19:12:52 +0800

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,27 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        local config = require("nvim-treesitter.configs")
-        config.setup({
-            ensure_installed = { "lua", "cpp", "c", "python", "c_sharp", "vim", "vimdoc" },
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            local config = require("nvim-treesitter.configs")
+            config.setup({
+                ensure_installed = { "lua", "cpp", "c", "python", "c_sharp", "vim", "vimdoc" },
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-refactor",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                refactor = {
+                    highlight_definitions = {
+                        enable = true,
+                        clear_on_cursor_move = true,
+                    }
+                }
+            })
+        end
+    },
 }
