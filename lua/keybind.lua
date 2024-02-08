883be439be4199e9738fd73b6776fc28fb3ca5e2
Ben Tang <sjps30503@gmail.com>
2024-02-08 22:40:30 +0800

    Barebone Bufremove interface

--- lua/keybind.lua
+++ lua/keybind.lua
@@ -19,6 +19,23 @@ vim.keymap.set("n", "L", ":bnext<CR>")
 vim.keymap.set("n", "<Tab>", ":bnext<CR>")
 vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>")
 
+-- Buffer Remove
+vim.keymap.set("n", "<leader>bd", function()
+    local bd = require("mini.bufremove").delete
+    if vim.bo.modified then
+
+        local choice = vim.fn.confirm(("Save chages to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
+        if choice == 1 then     -- Yes
+            vim.cmd.write()
+            bd(0)
+        elseif choice == 2 then -- No
+            bd(0, true)
+        end
+    else
+        bd(0)
+    end
+end, { desc = "Delete buffer", remap = false })
+
 -- Comment
 local comment = require("mini.comment")
 comment.setup({
