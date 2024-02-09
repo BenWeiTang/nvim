bf9bc744bec80ed694a86d14872cc01e3fc42950
Ben Tang <sjps30503@gmail.com>
2024-02-09 17:58:56 +0800

    Removed telescope-ui-select and use dressing instead

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,23 +10,24 @@ return {
                 },
             })
         end
-    },
-    {
-        "nvim-telescope/telescope-ui-select.nvim",
-        config = function()
-            require("telescope").setup({
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown {
-                        }
-                    }
-                },
-            })
-            local telescope = require("telescope")
-            telescope.load_extension("ui-select")
-            telescope.load_extension("fzf")
-        end
-    },
+    }
+    ,
+    -- {
+    --     "nvim-telescope/telescope-ui-select.nvim",
+    --     config = function()
+    --         require("telescope").setup({
+    --             extensions = {
+    --                 ["ui-select"] = {
+    --                     require("telescope.themes").get_dropdown {
+    --                     }
+    --                 }
+    --             },
+    --         })
+    --         local telescope = require("telescope")
+    --         telescope.load_extension("ui-select")
+    --         telescope.load_extension("fzf")
+    --     end
+    -- },
     {
         'nvim-telescope/telescope-fzf-native.nvim',
         build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
