035a6eb21f4c1c60f0bc8ecc1c2ff2910b2c11a7
Ben Tang <sjps30503@gmail.com>
2024-02-09 12:07:26 +0800

    Standardized to use double quotes

--- lua/keybind.lua
+++ lua/keybind.lua
@@ -1,11 +1,11 @@
 -- Basic Vim Key Remap
-vim.keymap.set('i', 'kj', '<esc>', { remap = false })
-vim.keymap.set('n', '<space>', ':w<cr>', { remap = false })
+vim.keymap.set("i", "kj", "<esc>", { remap = false })
+vim.keymap.set("n", "<space>", ":w<cr>", { remap = false })
 
-vim.keymap.set('n', '<A-h>', '<C-w>h', { desc = 'Go to left window', remap = true }) -- Honestly, not too sure if I want recursive mapping here...
-vim.keymap.set('n', '<A-j>', '<C-w>j', { desc = 'Go to lower window', remap = true })
-vim.keymap.set('n', '<A-k>', '<C-w>k', { desc = 'Go to upper window', remap = true })
-vim.keymap.set('n', '<A-l>', '<C-w>l', { desc = 'Go to right window', remap = true })
+vim.keymap.set("n", "<A-h>", "<C-w>h", { desc = "Go to left window", remap = true }) -- Honestly, not too sure if I want recursive mapping here...
+vim.keymap.set("n", "<A-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
+vim.keymap.set("n", "<A-k>", "<C-w>k", { desc = "Go to upper window", remap = true })
+vim.keymap.set("n", "<A-l>", "<C-w>l", { desc = "Go to right window", remap = true })
 
 vim.keymap.set("n", "<leader>ww", "<C-W>p", { desc = "Other window", remap = true })
 vim.keymap.set("n", "<leader>wd", "<C-W>c", { desc = "Delete window", remap = true })
@@ -18,6 +18,8 @@ vim.keymap.set("n", "H", ":bprevious<CR>")
 vim.keymap.set("n", "L", ":bnext<CR>")
 vim.keymap.set("n", "<Tab>", ":bnext<CR>")
 vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>")
+
+vim.keymap.set("n", "<leader>/", ":noh<CR>") -- Leader / to remove highlight, useful after searching a pattern
 
 -- Buffer Remove
 vim.keymap.set("n", "<leader>bd", function()
@@ -41,11 +43,11 @@ local comment = require("mini.comment")
 comment.setup({
     mappings = {
         -- Originally 'gc', but it's not really used so set to disabled
-        comment = '',
-        textobject = '',
+        comment = "",
+        textobject = "",
 
-        comment_line = '<A-/>',
-        comment_visual = '<A-/>',
+        comment_line = "<A-/>",
+        comment_visual = "<A-/>",
     }
 })
 
@@ -54,39 +56,39 @@ local cmp = require("cmp")
 cmp.setup({
     mapping = cmp.mapping.preset.insert({
         -- Default recommended mapping
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
 
         -- Custom mapping
-        ['<A-k>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
-        ['<A-j>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
+        ["<A-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+        ["<A-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
     }),
 })
 
 -- Debugging
 local dap = require("dap")
-vim.keymap.set('n', '<leader>dt', dap.toggle_breakpoint, {}) -- 'dt' means debug toggle
-vim.keymap.set('n', '<leader>dc', dap.continue, {})          -- 'dc' means debug continue
+vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {}) -- "dt" means debug toggle
+vim.keymap.set("n", "<leader>dc", dap.continue, {})          -- "dc" means debug continue
 
 -- Floating Terminal
-vim.keymap.set('n', '<leader>tt', '<CMD>lua require("FTerm").toggle()<CR>')
-vim.keymap.set('t', '<leader>tt', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
-vim.keymap.set('t', '<leader>td', '<C-\\><C-n><CMD>lua require("FTerm").exit()<CR>') -- Maybe I want to exit terminal upon closing as well...
+vim.keymap.set("n", "<leader>tt", "<CMD>lua require('FTerm').toggle()<CR>")
+vim.keymap.set("t", "<leader>tt", "<C-\\><C-n><CMD>lua require('FTerm').toggle()<CR>")
+vim.keymap.set("t", "<leader>td", "<C-\\><C-n><CMD>lua require('FTerm').exit()<CR>") -- Maybe I want to exit terminal upon closing as well...
 
 -- Leap
-vim.keymap.set('n', '<leader>s', '<Plug>(leap-forward)', {})
-vim.keymap.set('n', '<leader>S', '<Plug>(leap-backward)', {})
-vim.keymap.set('n', '<leader>gs', '<Plug>(leap-from-window)', {})
+vim.keymap.set({"n", "v"}, "<leader>s", "<Plug>(leap-forward)", {})
+vim.keymap.set({"n", "v"}, "<leader>S", "<Plug>(leap-backward)", {})
+vim.keymap.set("n", "<leader>gs", "<Plug>(leap-from-window)", {})
 
 -- Nvim-lspconfig
-vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
-vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
-vim.keymap.set('n', '<leader>rr', function()
+vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+vim.keymap.set("n", "<leader>rr", function()
     local position_params = vim.lsp.util.make_position_params()
     local new_name = vim.fn.input("New Name > ")
     if not new_name or new_name == "" then
@@ -122,12 +124,12 @@ vim.keymap.set('n', '<leader>rr', function()
 end, {})
 
 -- Neotree
-vim.keymap.set('n', '<a-n>', ':Neotree filesystem toggle left<cr>', {})
+vim.keymap.set("n", "<a-n>", ":Neotree filesystem toggle left<cr>", {})
 
 -- None-ls
-vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
+vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
 
 -- Telescope
 local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<a-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set("n", "<a-p>", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
