e2f958835638da9a0ce0dd6ebaaf5a6a29a0cea7
Ben Tang <sjps30503@gmail.com>
2024-02-09 21:48:20 +0800

    Update vim-config.lua typo

--- lua/vim-config.lua
+++ lua/vim-config.lua
@@ -10,7 +10,7 @@ vim.cmd('set ignorecase')
 -- Leader (Needs to be set before loading plugins)
 vim.g.mapleader = ','
 
--- I added these two lines here from (https://github.com/LunarVim/LunarVim/discussions/4418)
+-- I added these lines here from (https://github.com/LunarVim/LunarVim/discussions/4418)
 -- And the reason was to erase the weird background color in the snippet
 -- Not too sure if this works well on other themes and on other platforms
 vim.cmd("autocmd ColorScheme * highlight NormalFloat guifg=none guibg=none")
