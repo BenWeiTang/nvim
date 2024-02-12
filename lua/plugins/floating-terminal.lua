0c2f12019ef6692f8936ceac29228815c5bd1892
Ben Tang <sjps30503@gmail.com>
2024-02-12 15:07:29 +0800

    Update floating-terminal.lua

--- lua/plugins/floating-terminal.lua
+++ lua/plugins/floating-terminal.lua
@@ -2,7 +2,7 @@ return {
     'numToStr/FTerm.nvim',
     config = function()
         require("FTerm").setup({
-            cmd = "pwsh", -- Obviously want this to work on different platform as well
+            cmd = (require("os-config").GetName() == "win") and "pwsh" or "zsh",
         })
     end
 }
