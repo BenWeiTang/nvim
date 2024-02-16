1f75d372326355ce737f49aa3a16f08929a5e713
Ben Tang <sjps30503@gmail.com>
2024-02-16 13:10:52 +0800

    WIP codelldb hooked up but closes immediately

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,23 +1,72 @@
 return {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-        "rcarriga/nvim-dap-ui",
+    {
+        "jay-babu/mason-nvim-dap.nvim",
+        dependencies = "williamboman/mason.nvim",
+        cmd = { "DapInstall", "DapUninstall" },
+        config = function()
+            require("mason-nvim-dap").setup({
+                ensure_installed = { "codelldb", },
+                automatic_installation = false,
+            })
+        end,
     },
-    config = function()
-        local dap = require("dap")
-        local dapui = require("dapui")
+    {
+        "mfussenegger/nvim-dap",
+        dependencies = {
+            "rcarriga/nvim-dap-ui",
+        },
+        config = function()
+            local dap = require("dap")
+            local dapui = require("dapui")
+            dapui.setup()
 
-        dap.listeners.before.attach.dapui_config = function()
-            dapui.open()
+            dap.listeners.before.attach.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.launch.dapui_config = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated.dapui_config = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited.dapui_config = function()
+                dapui.close()
+            end
+            -- dap.listeners.after.event_initialized["dapui_config"] = function()
+            --     dapui.open()
+            -- end
+            -- dap.listeners.before.event_terminated["dapui_config"] = function()
+            --     dapui.close()
+            -- end
+            -- dap.listeners.before.event_exited["dapui_config"] = function()
+            --     dapui.close()
+            -- end
+
+            dap.adapters.codelldb = {
+                type = 'server',
+                port = "${port}",
+                executable = {
+                    -- Needs Mac support...
+                    -- command only takes absolute path
+                    command = vim.fn.stdpath("data") .. "\\mason\\bin\\codelldb.cmd",
+                    args = { "--port", "${port}" },
+
+                    -- On windows you may have to uncomment this:
+                    detached = false,
+                }
+            }
+            dap.configurations.cpp = {
+                {
+                    name = "Launch file",
+                    type = "codelldb",
+                    request = "launch",
+                    program = function()
+                        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+                    end,
+                    cwd = '${workspaceFolder}',
+                    stopOnEntry = false,
+                },
+            }
         end
-        dap.listeners.before.launch.dapui_config = function()
-            dapui.open()
-        end
-        dap.listeners.before.event_terminated.dapui_config = function()
-            dapui.close()
-        end
-        dap.listeners.before.event_exited.dapui_config = function()
-            dapui.close()
-        end
-    end
+    },
 }
