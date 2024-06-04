3853b9f2e5ad6f3eb88520ff0c3024c8273e4f35
Ben Tang <78770681+BenWeiTang@users.noreply.github.com>
2024-06-04 19:31:24 +0800

    Update completions.lua
    Added dependencies plugins and remove unused sources for cmp

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -3,10 +3,13 @@ return {
         "hrsh7th/nvim-cmp",
         dependencies = {
             "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-nvim-lua",
+            "hrsh7th/cmp-cmdline",
             "saadparwaiz1/cmp_luasnip",
             "rafamadriz/friendly-snippets",
             "L3MON4D3/LuaSnip",
-            "hrsh7th/cmp-cmdline",
         },
         config = function()
             local cmp = require 'cmp'
@@ -15,19 +18,20 @@ return {
             cmp.setup({
                 snippet = {
                     expand = function(args)
-                        require('luasnip').lsp_expand(args.body)
+                        require("luasnip").lsp_expand(args.body)
                     end,
                 },
                 window = {
                     completion = cmp.config.window.bordered(),
                     documentation = cmp.config.window.bordered(),
                 },
-                sources = cmp.config.sources({
-                    { name = 'nvim_lsp' },
-                    { name = 'luasnip' },
-                }, {
-                    { name = 'buffer' },
-                }),
+                sources = {
+                    { name = "nvim_lsp" },
+                    { name = "nvim_lua" }, -- this plugin/source knows to enable itself only inside lua
+                    { name = "buffer" , keyword_length = 5},
+                    { name = "path" },
+                    { name = "nvim_lsp" }
+                },
 
                 -- See: https://github.com/p00f/clangd_extensions.nvim/blob/main/README.md#completion-scores
                 sorting = {
