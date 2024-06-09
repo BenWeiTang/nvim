df87fc5055089bb3f00889cc0b1ec73adb6c6197
Ben Tang <78770681+BenWeiTang@users.noreply.github.com>
2024-06-09 16:21:33 +0800

    Update path.lua

--- lua/path.lua
+++ lua/path.lua
@@ -1,11 +1,16 @@
 local M = {}
 
 function M.GetCurrentFileName()
-    local abs = vim.api.nvim_buf_get_name(0)
+    local abs = M.GetCurrentAbsPath()
     abs = vim.fs.normalize(abs)
     local ret = vim.split(abs, "/", { plain=true })
     ret = ret[#ret]
     return ret
+end
+
+function M.GetCurrentAbsPath()
+    local abs = vim.api.nvim_buf_get_name(0)
+    return abs
 end
 
 return M
