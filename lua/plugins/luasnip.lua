88abd29912b6b2d3645ed3ab627c365b817dc6f2
Ben Tang <78770681+BenWeiTang@users.noreply.github.com>
2024-06-09 14:18:47 +0800

    Update luasnip.lua
    Added a bunch of snippets for unreal engine C++

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -26,39 +26,75 @@ return {
 
         if require("project-env-config").GetIsUnrealProject() then
             ls.add_snippets("all", {
-                s("ufn", fmt([[
-            UFUNCTION({finish})
-            {ret} {name}({params}){tail};
-            ]], {
-                    ret = i(1, "void"),
-                    name = i(2, "MyFunction"),
-                    params = i(3, ""),
-                    tail = c(4, { t(""), t(" const"), t(" override"), t(" final") }),
-                    finish = i(0, "")
-                })),
+                s("ufunction", fmt([[
+                    UFUNCTION({finish})
+                    {ret} {name}({params}){tail};
+                    ]], {
+                        ret     = i(1, "void"),
+                        name    = i(2, "MyFunction"),
+                        params  = i(3, ""),
+                        tail    = c(4, { t(""), t(" const"), t(" override"), t(" final") }),
+                        finish  = i(0, "")
+                    })
+                ),
 
-                s("ubf",
-                    { c(1,
-                        { t("BlueprintCallable"), t("BlueprintPure"), t("BlueprintNativeEvent"), t(
-                        "BlueprintImplementableEvent") }) }),
+                s("uproperty", fmt([[
+                    UPROPERTY({finish})
+                    {type} {name};
+                    ]], {
+                        type    = i(1, "Type"),
+                        name    = i(2, "Identifier"),
+                        finish  = i(0, "")
+                    })
+                ),
 
-                s("upr", fmt([[
-            UPROPERTY({finish})
-            {type} {name};
-            ]], {
-                    type = i(1, "Type"),
-                    name = i(2, "Identifier"),
-                    finish = i(0, "")
-                })),
+                s("ustruct", fmt([[
+                    USTRUCT()
+                    struct {API} F{name}
+                    {
+                        GENERATED_USTRUCT_BODY()
+                        {finish}
+                    };
+                    ]], {
+                        name    = i(1, "Identifier"),
+                        API     = i(2, "API_NAME"),
+                        finish  = i(0, "")
+                    })
+                ),
 
-                s("uvis",
-                    { c(1,
-                        { t("VisibleAnywhere"), t("VisibleDefaultsOnly"), t("VisibleInstanceOnly"), t("EditAnywhere"), t(
-                        "EditDefaultsOnly"), t("EditInstanceOnly"), t("") }) }),
-                s("ubp", { c(1, { t("BlueprintReadOnly"), t("BlueprintReadWrite"), t("BlueprintAssignable"), t("") }) }),
-                s("category", { t("Category = \""), i(1, "Category"), t("\"") }),
-                s("meta", { t("meta = ("), i(1, ""), t(")") }),
-                s("AllowPrivateAccess", { t("AllowPrivateAccess=true") }),
+                -- UFUNCTION execution accessibility
+                s("blueprintcallable",              { t("BlueprintCallable") }),
+                s("blueprintpure",                  { t("BlueprintPure") }),
+                s("blueprintnativeevent",           { t("BlueprintNativeEvent") }),
+                s("blueprintimplementableevent",    { t("BlueprintImplementableEvent") }),
+
+                -- UPROPERTY Blueprint Graph accessibility
+                s("blueprintreadonly",      { t("BlueprintReadOnly") }),
+                s("blueprintreadwrite",     { t("BlueprintReadWrite") }),
+                s("blueprintassignable",    { t("BlueprintAssignable") }),
+
+                -- UPROPERTY Details panel visibility
+                s("visibleanywhere",        { t("VisibleAnywhere") }),
+                s("visibledefaultsonly",    { t("VisibleDefaultsOnly") }),
+                s("visibleinstanceonly",    { t("VisibleInstanceOnly") }),
+
+                -- UPROPERTY Details panel editability
+                s("editanywhere",       { t("EditAnywhere") }),
+                s("editdefaultsonly",   { t("EditDefaultsOnly") }),
+                s("editinstanceonly",   { t("EditInstanceOnly") }),
+
+                -- UCLASS and USTRUCT Blueprint-ability
+                s("blueprinttype", { t("BlueprintType") }),
+                s("blueprintable", { t("Blueprintable") }),
+
+                -- Miscellaneous
+                s("category",           { t("Category=\""), i(1, "Category"), t("\"") }),
+                s("meta",               { t("meta=("), i(1, ""), t(")") }),
+                s("allowprivateaccess", { t("AllowPrivateAccess=true") }),
+                s("tobjptr",            { t("TObjectPtr<"), i(1, ""), t(">"), i(0, "") }),
+                s("tsharedptr",         { t("TSharedPtr<"), i(1, ""), t(">"), i(0, "") }),
+                s("cast",               { t("Cast<"), i(1, "TO"), t(", "), i(2, "FROM"), t(">("), i(3, ""), t(")"), i(0, "") }),
+                s("gcsfrontendapi",     { t("GCS_FRONTEND_API") }), -- project-based, change later
             })
         end
     end,
