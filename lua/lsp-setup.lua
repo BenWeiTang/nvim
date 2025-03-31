730131029075615c2388e3ba230adf792df6a63e
Ben Tang <ben.tang@lungteh.com>
2025-03-31 16:46:45 +0800

    Temporarily Disabled Builtin Auto-Cmp

--- lua/lsp-setup.lua
+++ lua/lsp-setup.lua
@@ -2,14 +2,14 @@ vim.lsp.enable({ "clangd", "luals" })
 
 -- One command applicable for multiple LSPs (?)
 -- https://gpanders.com/blog/whats-new-in-neovim-0-11/#builtin-auto-completion
-vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function(ev)
-        local client = vim.lsp.get_client_by_id(ev.data.client_id)
-        if client:supports_method('textDocument/completion') then
-            vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-        end
-    end,
-})
+-- vim.api.nvim_create_autocmd("LspAttach", {
+--     callback = function(ev)
+--         local client = vim.lsp.get_client_by_id(ev.data.client_id)
+--         if client:supports_method('textDocument/completion') then
+--             vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+--         end
+--     end,
+-- })
 
 -- Auto complete
 vim.cmd("set completeopt+=noselect")
