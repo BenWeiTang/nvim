489756b9406aa944283289d25c732ae2325c9016
Ben Tang <ben.tang@lungteh.com>
2025-03-31 14:16:11 +0800

    Refactored lsp-config.lua and Renamed it to mason.lsp
    1. Removed plugin 'nvim-lspconfig'
    2. Removed plugin 'mason-lspconfig'
    3. Renamed the file to 'mason.lua' since that is the only plugin that is
    in there

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup({
+                PATH = "prepend",
+            })
+        end
+    }
+}
