2b96f988cba39fca86cfdb59745f0fd50dcf5f8f
Ben Tang <ben.tang@lungteh.com>
2025-03-31 08:28:04 +0800

    Added comments in mini-move.lua

--- lua/plugins/mini-move.lua
+++ lua/plugins/mini-move.lua
@@ -2,3 +2,7 @@ return {
     'echasnovski/mini.move',
     version = false,
 }
+
+-- This plugin allows you to move selected texts around
+-- Note: The 'setup()' function is call in 'keybind.lua' because
+-- I also need to set up a few keybinds
