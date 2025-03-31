618ebf79e690b79eac22564c29fc0e27bd0a8d35
Ben Tang <ben.tang@lungteh.com>
2025-03-31 13:37:46 +0800

    Basic LSP Setup for Lua
    1. Removed Lua-related logics in 'lsp-config.lua'
    2. Added logics to start auto completion in 'lsp-setup.lua'

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -26,34 +26,6 @@ return {
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
             local config = require("lspconfig")
 
-            config.lua_ls.setup({
-                capabilities = capabilities,
-
-                -- Found the following setup from:
-                -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#lua_ls
-                on_init = function(client)
-                    local path = client.workspace_folders[1].name
-                    if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
-                        client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
-                            Lua = {
-                                runtime = {
-                                    version = 'LuaJIT'
-                                },
-                                workspace = {
-                                    checkThirdParty = false,
-                                    library = {
-                                        vim.env.VIMRUNTIME
-                                    }
-                                }
-                            }
-                        })
-
-                        client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-                    end
-                    return true
-                end
-            })
-
             local clangd_path = "clangd"
             local proj_env = require("project-env-config")
             if proj_env.GetIsUnrealProject() then
