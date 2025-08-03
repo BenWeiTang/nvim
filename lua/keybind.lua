3dcf69b87748137fdf717279b251b9d613310af8
Ben Tang <sjps30503@gmail.com>
2025-08-03 19:28:24 +0800

    Add plugin flash.nvim

--- lua/keybind.lua
+++ lua/keybind.lua
@@ -139,6 +139,14 @@ vim.keymap.set("n", "<leader>fw", function()
     builtin.grep_string({ search = vim.fn.expand("<cword>") })
 end, { desc = "Fuzzy Find Word Under Cursor" })
 
+-- Flash
+local flash = require("flash")
+vim.keymap.set({ "n", "x", "o" },   "s",    function() flash.jump() end,                {desc = "Flash"})
+vim.keymap.set({ "n", "x", "o" },   "S",    function() flash.treesitter() end,          {desc = "Flash Treesitter"})
+vim.keymap.set({ "o" },             "r",    function() flash.remote() end,              {desc = "Remote Flash"})
+vim.keymap.set({ "o", "x" },        "R",    function() flash.treesitter_search() end,   {desc = "Treesitter Search"})
+vim.keymap.set({ "c" },  ThumbCombo("s"),   function() flash.toggle() end,              {desc = "toggle Flash Search"})
+
 -- Mini Move
 local mini_move = require("mini.move")
 mini_move.setup({
