fd1675d4b01e0477c3dd80aaac03338b7ea4424c
Ben Tang <sjps30503@gmail.com>
2025-10-26 11:53:29 +0800

    Update lualine
    1. Update theme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
     config = function()
         require("lualine").setup({
             options = {
-                theme = 'iceberg_dark'
+                theme = 'tomorrow_night'
             },
             sections = {
                 lualine_c = {
