4c206fca72baba82ccf2b5ec5546adf670a5de63
bt <ben.tang@lungteh.com>
2026-02-04 09:34:19 +0800

    Update nvim-treesitter branch
    1. Specify it to stay on `master`

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
+        branch = "master",
         config = function()
             local config = require("nvim-treesitter.configs")
             config.setup({
