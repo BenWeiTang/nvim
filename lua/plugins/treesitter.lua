bc806be8aa6461ccfea4b527576d0a27d0ca488f
Ben Tang <ben.tang@lungteh.com>
2026-04-07 11:12:46 +0800

    Update treesitter.lua
    1. Add autocmd for vue files with explanations

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -10,6 +10,21 @@ return {
                 highlight = { enable = true },
                 indent = { enable = true },
             })
+
+            -- This forces Treesitter to start the parser for the current buffer, if it's a vue file.
+            --
+            -- Native treesitter has gotten better at identifying the various languages in a vue file.
+            -- Also, newer versions of commentstring has also gotten better at using treesitter
+            -- parsers to find the current comment style.
+            -- The only issue is that treesitter is not automatically started when a buffer is
+            -- loaded. Therefore, for a vue file, upon loading it into a buffer, we force start
+            -- treesitter to start parsing and the commenting should come naturally.
+            vim.api.nvim_create_autocmd("FileType", {
+                pattern = "vue",
+                callback = function()
+                    vim.treesitter.start()
+                end,
+            })
         end
     },
     {
