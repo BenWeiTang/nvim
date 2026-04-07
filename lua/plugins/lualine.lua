3f889cc6c7f605c1ada0ca4e7a7c62c44908d3c9
Ben Tang <ben.tang@lungteh.com>
2026-04-07 11:32:30 +0800

    Clean up lualine
    1. Replace `config` with `opts`
    2. Remove comment

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,27 +1,19 @@
 return {
     "nvim-lualine/lualine.nvim",
-    config = function()
-        require("lualine").setup({
-            options = {
-                -- theme = 'tomorrow_night',
-                theme = 'iceberg_dark',
-                component_separators = { left = '', right = ''},
-                section_separators = { left = '', right = ''},
-            },
-            sections = {
-                lualine_c = {
-                    {
-                        'buffers',
-                        icons_enabled = false,
-                        symbols = {
-                            alternate_file = '',
-                        },
-                        -- buffers_color = {
-                            -- inactive = 'lualine_c_inactive'
-                        -- }
-                    },
+    opts = {
+        options = {
+            theme = 'iceberg_dark',
+            component_separators = { left = '', right = ''},
+            section_separators = { left = '', right = ''},
+        },
+        sections = {
+            lualine_c = {
+                {
+                    'buffers',
+                    icons_enabled = false,
+                    symbols = { alternate_file = '' },
                 }
             },
-        })
-    end
+        }
+    },
 }
