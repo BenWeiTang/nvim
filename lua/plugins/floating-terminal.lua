6f4729e5d384618d97a4ecfdaacc2b518bbdc8f5
Ben Tang <ben.tang@lungteh.com>
2026-04-07 11:44:33 +0800

    Clean up floating-terminal
    1. Remove comment
    2. Replace `config` with `opts`

--- lua/plugins/floating-terminal.lua
+++ lua/plugins/floating-terminal.lua
@@ -1,9 +1,11 @@
 return {
     'numToStr/FTerm.nvim',
-    config = function()
-        require("FTerm").setup({
-            -- cmd = (require("os-config").GetName() == "win") and "pwsh" or "zsh",
-            cmd = require("os-config").GetShellName(),
-        })
-    end
+    opts = {
+        cmd = require("os-config").GetShellName()
+    }
+    -- config = function()
+    --     require("FTerm").setup({
+    --         cmd = require("os-config").GetShellName(),
+    --     })
+    -- end
 }
