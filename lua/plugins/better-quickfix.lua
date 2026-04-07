cf6aa5a4b2a4db7ed235abb1ce38574660a48b2f
Ben Tang <ben.tang@lungteh.com>
2026-04-07 11:50:30 +0800

    Clean up better-quickfix
    1. Replace `config` with `opts`

--- lua/plugins/better-quickfix.lua
+++ lua/plugins/better-quickfix.lua
@@ -1,13 +1,11 @@
 return {
     'kevinhwang91/nvim-bqf',
     ft = 'qf',
-    config = function()
-        require("bqf").setup({
-            auto_enable = true,
-            auto_resize_height = true,
-            preview = {
-                show_title = false,
-            }
-        })
-    end
+    opts = {
+        auto_enable = true,
+        auto_resize_height = true,
+        preview = {
+            show_title = false,
+        }
+    }
 }
