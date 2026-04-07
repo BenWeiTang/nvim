56a1d36f35d85997b7d04af713265dc9e236b605
Ben Tang <ben.tang@lungteh.com>
2026-04-07 11:21:26 +0800

    Clean up flash
    1. Remove comment in `flash.lua`
    2. Comment out unused commands in `keybind.lua`

--- lua/keybind.lua
+++ lua/keybind.lua
@@ -129,11 +129,11 @@ end, { desc = "Fuzzy Find Word Under Cursor" })
 
 -- Flash
 local flash = require("flash")
-vim.keymap.set({ "n", "x", "o" },   "s",    function() flash.jump() end,                {desc = "Flash"})
-vim.keymap.set({ "n", "x", "o" },   "S",    function() flash.treesitter() end,          {desc = "Flash Treesitter"})
-vim.keymap.set({ "o" },             "r",    function() flash.remote() end,              {desc = "Remote Flash"})
-vim.keymap.set({ "o", "x" },        "R",    function() flash.treesitter_search() end,   {desc = "Treesitter Search"})
-vim.keymap.set({ "c" },  ThumbCombo("s"),   function() flash.toggle() end,              {desc = "toggle Flash Search"})
+vim.keymap.set({ "n", "x", "o" },   "s",  function() flash.jump() end,              { desc = "Flash"} )
+-- vim.keymap.set({ "n", "x", "o" },   "S",  function() flash.treesitter() end,        { desc = "Flash Treesitter"} )
+-- vim.keymap.set({ "o" },             "r",  function() flash.remote() end,            { desc = "Remote Flash"} )
+-- vim.keymap.set({ "o", "x" },        "R",  function() flash.treesitter_search() end, { desc = "Treesitter Search"} )
+-- vim.keymap.set({ "c" },  ThumbCombo("s"), function() flash.toggle() end,            { desc = "toggle Flash Search"} )
 
 -- Mini Move
 local mini_move = require("mini.move")
